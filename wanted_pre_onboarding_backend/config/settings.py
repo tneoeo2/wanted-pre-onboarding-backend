c98c7d9aace827bf178c3497523b5c93d0730a3b
tneoeo2 <tneoeo2@gmail.com>
2023-10-18 23:13:30 +0900

    feat: setting 변경

--- wanted_pre_onboarding_backend/config/settings.py
+++ wanted_pre_onboarding_backend/config/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'rest_framework',
+    'accounts',
+    'recruit',
 ]
 
 MIDDLEWARE = [
